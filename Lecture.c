610483c7f1d476285fa0646bf1ffe0c218ec96a0
Arch2jz <gurnoorskullz@gmail.com>
2026-01-30 11:24:52 +0530

    First commit biatch

---
+++ Lecture.c
@@ -0,0 +1,76 @@
+// #include <stdio.h>
+// int main(){
+
+//     printf("Epstien 😋");
+//     return 0;
+// }
+
+//1. Add two numbers
+// #include <stdio.h>
+// int main() {
+//     int a, b;
+//     scanf("%d %d", &a, &b);
+//     printf("Sum = %d", a + b);
+//     return 0;
+// }
+
+// //2. Remainder of two numbers
+// #include <stdio.h>
+// int main() {
+//     int a, b;
+//     scanf("%d %d", &a, &b);
+//     printf("Remainder = %d", a % b);
+//     return 0;
+// }
+
+// //3. Product of two numbers
+// #include <stdio.h>
+// int main() {
+//     int a, b;
+//     scanf("%d %d", &a, &b);
+//     printf("Product = %d", a * b);
+//     return 0;
+// }
+
+// //4. Average of three numbers
+// #include <stdio.h>
+// int main() {
+//     int a, b, c;
+//     scanf("%d %d %d", &a, &b, &c);
+// }
+
+// // 20. Bitwise AND
+// #include <stdio.h>
+// int main() {
+//     int a, b;
+//     scanf("%d %d", &a, &b);
+//     printf("%d", a & b);
+//     return 0;
+// }
+
+// // 21. Bitwise OR
+// #include <stdio.h>
+// int main() {
+//     int a, b;
+//     scanf("%d %d", &a, &b);
+//     printf("%d", a | b);
+//     return 0;
+// }
+
+// 18. Largest of two numbers
+#include <stdio.h>
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", (a > b) ? a : b);
+    return 0;
+}
+
+// // 19. Even or Odd
+// #include <stdio.h>
+// int main() {
+//     int n;
+//     scanf("%d", &n);
+//     printf("%s", (n % 2 == 0) ? "Even" : "Odd");
+//     return 0;
+// }
